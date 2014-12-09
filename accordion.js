d438068ce19535509452c0e54e4f9b51f0d71f71
Karl Gustav Roksund <Karl.Gustav.Roksund@pearson.com>
2014-12-09 08:40:01 +0100

    Initial commit

---
+++ accordion.js
@@ -0,0 +1,48 @@
+'use strict';
+
+angular.module('app', [])
+    .directive('slideToggle', function() {
+        return {
+            restrict: 'AE',
+            link: function(scope, element, attrs) {
+                var target = element.parent().find('slideable')[0];
+                attrs.expanded = false;
+                element.bind('click', function() {
+                    var content = target.querySelector('.slideable_content');
+                    if(!attrs.expanded) {
+                        content.style.border = '1px solid rgba(0,0,0,0)';
+                        var y = content.clientHeight;
+                        content.style.border = 0;
+                        target.style.height = y + 'px';
+                    } else {
+                        target.style.height = '0px';
+                    }
+                    attrs.expanded = !attrs.expanded;
+                });
+            }
+        }
+    })
+    .directive('slideable', function () {
+        return {
+            restrict:'AE',
+            compile: function (element, attr) {
+                // wrap tag
+                var contents = element.html();
+                element.css({display: 'block'});
+                element.html('<div class="slideable_content" style="margin:0 !important; padding:0 !important" >' + contents + '</div>');
+
+                return function postLink(scope, element, attrs) {
+                    // default properties
+                    attrs.duration = (!attrs.duration) ? '1s' : attrs.duration;
+                    attrs.easing = (!attrs.easing) ? 'ease-in-out' : attrs.easing;
+                    element.css({
+                        'overflow': 'hidden',
+                        'height': '0px',
+                        'transitionProperty': 'height',
+                        'transitionDuration': attrs.duration,
+                        'transitionTimingFunction': attrs.easing
+                    });
+                };
+            }
+        };
+    });
